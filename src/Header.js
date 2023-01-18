eaa9bde3c7b2e5f65f349e4b5532791af6503e1a
Praveen <2pkashyap2001@gmail.com>
2023-01-18 22:33:42 +0530

    resolve

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Header.css'
 import logo from 'C:\\Users\\hp\\OneDrive\\Desktop\\PROJECTS\\Airbnb\\RENT\\rental-app\\src\\images\\Airbnb-logo.png'
+// import SearchIcon from "@material-ui/icons/Search";
+// import SearchIcon from '@material-ui/icons/Search';
+// import SearchIcon from '@mui/icons-material/Search';
 function Header() {
   return (
     <div className='header'>
            <img className='header_icon' src={logo}  alt='logo'/>
            <div className='header_center'>
+            bhkbjbl
                 <input type='text'></input>
+                {/* <SearchIcon/> */}
            </div>
 
     </div> 
